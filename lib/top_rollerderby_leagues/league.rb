30d40f353e92751a3ff2c5f187c7c13c78fb148b
Amanda Wehrman <arwehrman@gmail.com>
2017-09-07 19:07:44 -0400

    stubbed out code for League

--- lib/top_rollerderby_leagues/league.rb
+++ lib/top_rollerderby_leagues/league.rb
@@ -2,19 +2,41 @@ class TopRollerderbyLeagues::League
 
   attr_accessor :name, :location, :rank, :url
 
+  #League object- attributes name, location, rank, url
+  #need to create leagues with scrape
+  #need to create list of leagues ranked 1- 10
+  #need to create league profile: name, rank, location, url
 
+  @@all = []
 
+#instance method
+  def initialize (name, location, rank, url)
+    @name = name
+    @location = location
+    @rank = rank
+    @url = url
+    self.class.all << self
+  end
 
+#class method
+  def self.all
+    @@all
+  end
 
+#class method
+#string data currently- should pull from League attr
+  def self.league_profile
+    puts <<~DOC
+    name
+    rank
+    location
+    url
+    DOC
+  end
 
-
-
-
-
-
-
-#get this data from the hash
-  def leagues_list
+#class method
+#string data currently- should pull from League attr
+  def self.leagues_list
     puts ""
     puts <<~DOC
     1. Victorian Roller Derby
@@ -30,6 +52,11 @@ class TopRollerderbyLeagues::League
     DOC
   end
 
-
-
 end #ends class
+
+
+#test data
+TopRollerderbyLeagues::League.new("Angel City", "Los Angeles, CA, US", "4", "https://wftda.com/wftda-leagues/angel-city-derby-girls/")
+TopRollerderbyLeagues::League.new("Gotham Roller Derby", "New York City, NY, US", "3", "https://wftda.com/wftda-leagues/gotham-girls-roller-derby/")
+TopRollerderbyLeagues::League.new("Victorian Roller Derby", "Melbourne, VIC, AU", "1", "https://wftda.com/wftda-leagues/victorian-roller-derby-league/")
+TopRollerderbyLeagues::League.new("Rose City Rollers", "Portland, OR, US" ,"2", "https://wftda.com/wftda-leagues/rose-city-rollers/")
--- lib/top_rollerderby_leagues/wftda_scraper.rb
+++ lib/top_rollerderby_leagues/wftda_scraper.rb
@@ -2,17 +2,18 @@ class WftdaScraper
 
   attr_accessor :name, :location, :rank, :url
 
+#class method
   def self.scrape_wftda
     league_array = []
     doc = Nokogiri::HTML(open("https://wftda.com/wftda-leagues/"))
     doc.search(".league_type-member").each do |league|
-      league_hash = {
-      :name => league.search("span.league_name").text.strip,
-      :rank => league.search("span.ranking").text.strip,
-      :location => league.search("div.league-meta.league-location").text.strip,
-      :url => league.search("div.league-logo a").attr("href").text.strip,
-    }
-    league_array << league_hash
+      member = League.new
+      member.name = league.search("span.league_name").text.strip
+      member.rank = league.search("span.ranking").text.strip
+      member.location = league.search("div.league-meta.league-location").text.strip
+      member.url = league.search("div.league-logo a").attr("href").text.strip
+      member
+    league_array << member
     end
     league_array
   end
