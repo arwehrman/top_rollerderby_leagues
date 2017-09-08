03549b5d3c9fdaa5f0b419080147dbed424c6430
Amanda Wehrman <arwehrman@gmail.com>
2017-09-07 22:03:57 -0400

    scraping to self.new for now

--- lib/top_rollerderby_leagues/wftda_scraper.rb
+++ lib/top_rollerderby_leagues/wftda_scraper.rb
@@ -3,17 +3,17 @@ class WftdaScraper
   attr_accessor :name, :location, :rank, :url
 
 #class method
-# hash into an array- not sure about this keeping for now
+# instance into an array- not sure about this keeping for now
 # using self.new but probably should change to League.new
   def self.scrape_wftda
     league_array = []
     doc = Nokogiri::HTML(open("https://wftda.com/wftda-leagues/"))
-    doc.search(".league_type-member").each do |league|
+    doc.search(".league_type-member").each do |league_info|
       member = self.new
-      member.name = league.search("span.league_name").text.strip
-      member.rank = league.search("span.ranking").text.strip
-      member.location = league.search("div.league-meta.league-location").text.strip
-      member.url = league.search("div.league-logo a").attr("href").text.strip
+      member.name = league_info.search("span.league_name").text.strip
+      member.rank = league_info.search("span.ranking").text.strip
+      member.location = league_info.search("div.league-meta.league-location").text.strip
+      member.url = league_info.search("div.league-logo a").attr("href").text.strip
       member
     league_array << member
     end
