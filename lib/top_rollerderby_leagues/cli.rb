434df8d9b5e664a06558d3d96ddfd40e0079d127
Amanda Wehrman <arwehrman@gmail.com>
2017-09-09 15:15:56 -0400

    fixed white spacing and formatting of code

--- lib/top_rollerderby_leagues/cli.rb
+++ lib/top_rollerderby_leagues/cli.rb
@@ -1,52 +1,44 @@
 class TopRollerderbyLeagues::CLI
 
+  def call
 
+    list_leagues
+    menu
+    later
+  end
 
+  def list_leagues
+    TopRollerderbyLeagues::League.top_ten_leagues_list
+  end
 
-      def call
+  def league_profile
+    @profile = TopRollerderbyLeagues::League.league_profile
+  end
 
-        list_leagues
-        menu
-        later
+  def menu
+    puts ""
+    puts "Welcome to the WFTDA League List"
+    puts ""
+    puts "Please enter rank number to see more information on league, list  or exit : "
+    input = nil
+    while input != "exit"
+    input = gets.strip.downcase
+    case
+    when input == "exit"
+      return
+    when input == "list"
+      list_leagues
+    when input.to_i >=1 && input.to_i <=10
+      TopRollerderbyLeagues::League.league_profile_rank(input.to_i)
+    else
+      puts "Invalid entry. Please enter rank number, list or exit: "
       end
+    end
+  end
 
-#working  with fake data
-      def list_leagues
-        TopRollerderbyLeagues::League.top_ten_leagues_list
-      end
+  def later
+    puts ""
+    puts "Thanks for checking out WFTDA League list"
+  end
 
-#working with fake data
-      def league_profile
-        @profile = TopRollerderbyLeagues::League.league_profile
-      end
-
-
-#probably change to an if statement
-#need to pull league profile when rank number is selected
-      def menu
-        puts ""
-        puts "Welcome to the WFTDA League List"
-        puts ""
-        puts "Please enter rank number to see more information on league, list  or exit : "
-        input = nil
-        while input != "exit"
-        input = gets.strip.downcase
-        case
-        when input == "exit"
-          return
-        when input == "list"
-          list_leagues
-        when input.to_i >=1 && input.to_i <=10
-          TopRollerderbyLeagues::League.league_profile_rank(input.to_i)
-        else
-          puts "Invalid entry. Please enter rank number, list or exit: "
-        end
-      end
-
-
-      def later
-        puts ""
-        puts "Thanks for checking out WFTDA League list"
-      end
-
-  end #end of class
+end
--- lib/top_rollerderby_leagues/wftda_scraper.rb
+++ lib/top_rollerderby_leagues/wftda_scraper.rb
@@ -1,12 +1,8 @@
 class WftdaScraper
 attr_accessor :name, :location, :rank, :url
 
-
-
   def self.scrape_wftda
-    #league_array = []
-  
-    doc = Nokogiri::HTML(open("http://wftda.com/wftda-leagues/"))
+    doc = Nokogiri::HTML(open("https://wftda.com/wftda-leagues/"))
     doc.search(".league_type-member").each do |league_info|
       member = TopRollerderbyLeagues::League.new
       member.name = league_info.search("span.league_name").text.strip
@@ -15,8 +11,6 @@ attr_accessor :name, :location, :rank, :url
       member.url = league_info.search("div.league-logo a").attr("href").text.strip
       member.save
       #URI.join(https://wftda.com,href).to_s
-      #league_array << member
     end
-    #league_array
   end
 end
